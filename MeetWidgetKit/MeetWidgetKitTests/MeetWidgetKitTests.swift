0310f94df2cf9c200d077f846aa9f64a4d68a820
seungchan2 <chanchu@hansung.ac.kr>
2024-06-17 13:14:27 +0900

    WidgetHelper 테스트 코드 작성

--- MeetWidgetKit/MeetWidgetKitTests/MeetWidgetKitTests.swift
+++ MeetWidgetKit/MeetWidgetKitTests/MeetWidgetKitTests.swift
@@ -6,31 +6,59 @@
 //
 
 import XCTest
+import Core
 @testable import MeetWidgetKit
 
-final class MeetWidgetKitTests: XCTestCase {
+import Quick
+import Nimble
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+final class WidgetHelperSpec: QuickSpec {
+    override func spec() {
+        var widgetHelper: WidgetHelper!
+        
+        beforeEach {
+            widgetHelper = WidgetHelper.shared
+        }
+        
+        describe("WidgetHelper") {
+            context("횟수 저장 및 불러옵니다.") {
+                
+                it("카운트된 횟수를 보여줍니다.") {
+                    widgetHelper.save(count: 10, kind: .calculate)
+                    
+                    let count = widgetHelper.load(kind: .calculate)
+                    expect(count).to(equal(10))
+                }
+                
+                it("+ 버튼 클릭 시, 횟수가 1 증가합니다.") {
+                    widgetHelper.save(count: 10, kind: .calculate)
+                    
+                    widgetHelper.calculateCount(operation: .sum)
+                    
+                    let count = widgetHelper.load(kind: .calculate)
+                    expect(count).to(equal(11))
+                }
+                
+                it("- 버튼 클릭 시, 횟수가 1 감소합니다.") {
+                    widgetHelper.save(count: 10, kind: .calculate)
+                    
+                    widgetHelper.calculateCount(operation: .minus)
+                    
+                    let count = widgetHelper.load(kind: .calculate)
+                    expect(count).to(equal(9))
+                }
+            }
+            
+            context("강아지 이미지 저장하고 불러옵니다.") {
+                it("fetch 버튼 클릭 시, 이미지를 불러오고 저장합니다.") {
+                    let image = UIImage(systemName: "star.fill")!
+                    widgetHelper.save(image: image, kind: .image)
+                    
+                    let loadedImage = widgetHelper.loadImage(kind: .image)
+                    expect(loadedImage).toNot(beNil())
+                    expect(loadedImage?.pngData()).to(equal(image.pngData()))
+                }
+            }
         }
     }
-
 }
