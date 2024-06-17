cbcddffddc0d811009e07f447550a023322f8abc
seungchan2 <chanchu@hansung.ac.kr>
2024-06-17 13:24:42 +0900

    WidgetHelper 테스트 context 수정

--- MeetWidgetKit/MeetWidgetKitTests/MeetWidgetKitTests.swift
+++ MeetWidgetKit/MeetWidgetKitTests/MeetWidgetKitTests.swift
@@ -12,6 +12,11 @@ import Core
 import Quick
 import Nimble
 
+/*
+ describe: Test Suite 그룹화하는 역할 (주로 테스트하려는 기능이나 특정 모듈에 대한 설명을 포함)
+ context: 테스트 케이스를 더 구체적인 상황이나 조건에 따라 그룹화하는 역할
+ it: 개별적인 테스트 케이스를 작성하는 곳
+ */
 final class WidgetHelperSpec: QuickSpec {
     override func spec() {
         var widgetHelper: WidgetHelper!
@@ -21,39 +26,32 @@ final class WidgetHelperSpec: QuickSpec {
         }
         
         describe("WidgetHelper") {
-            context("횟수 저장 및 불러옵니다.") {
-                
+            context("+, - 버튼 클릭하면") {
                 it("카운트된 횟수를 보여줍니다.") {
                     widgetHelper.save(count: 10, kind: .calculate)
-                    
                     let count = widgetHelper.load(kind: .calculate)
                     expect(count).to(equal(10))
                 }
                 
-                it("+ 버튼 클릭 시, 횟수가 1 증가합니다.") {
+                it("횟수가 1 증가합니다.") {
                     widgetHelper.save(count: 10, kind: .calculate)
-                    
                     widgetHelper.calculateCount(operation: .sum)
-                    
                     let count = widgetHelper.load(kind: .calculate)
                     expect(count).to(equal(11))
                 }
                 
-                it("- 버튼 클릭 시, 횟수가 1 감소합니다.") {
+                it("횟수가 1 감소합니다.") {
                     widgetHelper.save(count: 10, kind: .calculate)
-                    
                     widgetHelper.calculateCount(operation: .minus)
-                    
                     let count = widgetHelper.load(kind: .calculate)
                     expect(count).to(equal(9))
                 }
             }
-            
-            context("강아지 이미지 저장하고 불러옵니다.") {
-                it("fetch 버튼 클릭 시, 이미지를 불러오고 저장합니다.") {
+                        
+            context("fetch 버튼 클릭하면") {
+                it("강아지 이미지를 불러오고 저장합니다.") {
                     let image = UIImage(systemName: "star.fill")!
                     widgetHelper.save(image: image, kind: .image)
-                    
                     let loadedImage = widgetHelper.loadImage(kind: .image)
                     expect(loadedImage).toNot(beNil())
                     expect(loadedImage?.pngData()).to(equal(image.pngData()))
